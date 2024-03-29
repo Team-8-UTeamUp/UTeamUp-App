791b60cb47bc1ee97b2bbd24f470576dbc759b68
ehzrl <117248305+ehzrl@users.noreply.github.com>
2024-03-29 01:20:25 -0500

    Update Quiz_Page.js
    added project preference to quiz page

--- client/src/components/Quiz_Page.js
+++ client/src/components/Quiz_Page.js
@@ -1,6 +1,7 @@
 import {React, useState} from 'react'
 import Header from './Header.js'
 import axios from "axios"
+import Preference from './Project_Preferences/Preference.js';
 
 
 function Quiz_Handler() {
@@ -172,6 +173,7 @@ function Question3({studentId}) { // Project Preference: CS Project
         <>
             <div class="centered">
                 <p>Rank your top 5 CS Projects</p><br></br> 
+                <Preference/> 
 
             </div>
         </>
@@ -183,6 +185,7 @@ function Question4({studentId}) { // Project Preference: CS Project
         <>
             <div class="centered">
                 <p>Rank your top 5 UTD Design Projects</p><br></br> 
+                <Preference/> 
             
             </div>
         </>
