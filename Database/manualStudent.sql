5093b7f79ee9b28dd164cf4e00a4bf65218a0f5b
ASwift22 <118293043+ASwift22@users.noreply.github.com>
2024-04-11 22:47:18 -0500

    insert groupID value to some students manually

---
+++ Database/manualStudent.sql
@@ -0,0 +1,56 @@
+use testuteamup;
+
+update student
+set groupId =1
+where studentId in ('JBL269228','LRH283238','PVG223758');
+
+
+update student
+set groupId =3
+where studentId in ('DBR226066','JGB283737','KUW255249');
+
+
+update student
+set groupId =4
+where studentId in ('LGC298295','VGB210890');
+
+
+
+update student
+set groupId =5
+where studentId in ('VXR226945','YKR285019');
+
+
+
+update student
+set groupId =6
+where studentId in ('ABG222946','SYW291494');
+
+
+update student
+set groupId =7
+where studentId in ('CLG220400','RTS212693');
+
+
+
+update student
+set groupId =8
+where studentId in ('DTB222551','TNS217191');
+
+
+
+update student
+set groupId =9
+where studentId in ('SAA206256','VDT285679');
+
+
+update student
+set groupId =10
+where studentId in ('AHR277028','SMW210084');
+
+
+
+update student
+set groupId =11
+where studentId in ('CHP224906','EBC289816');
+
